c69a03392e8e70542a17f6671501181522ad3d89
Jaco Pretorius <jaco.pretorius@betterment.com>
2017-09-06 16:41:20 -0400

    Add tests

--- Fizzlefade/main.c
+++ Fizzlefade/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef void (*PIXEL_FUNC_PTR)(uint16_t, uint16_t);
 
@@ -31,10 +32,70 @@ void print_pixel(uint16_t x, uint16_t y) {
     printf("%d, %d\n", x, y);
 }
 
+// Test Functions
+
+static bool test_screen[320][200];
+
+void reset_test_screen() {
+    int x,y;
+    for(x=0;x<320;x++) {
+        for(y=0;y<200;y++) {
+            test_screen[x][y] = false;
+        }
+    }
+}
+
+void test_every_x_position_is_between_0_and_320(uint16_t x, uint16_t y) {
+    if (x < 0 || x >= 320) {
+        printf("test_every_x_position_is_between_0_and_320: %d was outside of the expected range\n", x);
+    }
+}
+
+void test_every_y_position_is_between_0_and_200(uint16_t x, uint16_t y) {
+    if (y < 0 || y >= 200) {
+        printf("test_every_y_position_is_between_0_and_200: %d was outside of the expected range\n", x);
+    }
+}
+
+void test_every_position_is_unique(uint16_t x, uint16_t y) {
+    if (test_screen[x][y]) {
+        printf("test_every_position_is_unique: %d, %d was returned multiple times\n", x, y);
+    } else {
+        test_screen[x][y] = true;
+    }
+}
+
+void test_every_possible_position_is_returned(uint16_t x, uint16_t y) {
+    test_screen[x][y] = true;
+}
+
+
+void run_all_tests() {
+    printf("Running tests.\n");
+    
+    fizzlefade(&test_every_x_position_is_between_0_and_320);
+    fizzlefade(&test_every_y_position_is_between_0_and_200);
+    
+    reset_test_screen();
+    fizzlefade(&test_every_position_is_unique);
+    
+    reset_test_screen();
+    fizzlefade(&test_every_possible_position_is_returned);
+    int x,y;
+    for(x=0;x<320;x++) {
+        for(y=0;y<200;y++) {
+            if (!test_screen[x][y]) {
+                printf("test_every_possible_position_is_returned: %d, %d was never returned\n", x, y);
+            }
+        }
+    }
+    
+    printf("All done.\n");
+}
+
+// End of Test Functions
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Starting fade...\n");
-    fizzlefade(&print_pixel);
-    printf("All done...\n");
+    run_all_tests();
     return 0;
 }

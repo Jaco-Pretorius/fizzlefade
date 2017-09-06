5208bfa2b72317763a0026118a250ba33ca23019
Jaco Pretorius <jaco.pretorius@betterment.com>
2017-09-06 14:41:46 -0400

    Initial Commit

---
+++ Fizzlefade/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Fizzlefade
+//
+//  Created by Jaco Pretorius on 9/6/17.
+//  Copyright © 2017 Jaco Pretorius. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
